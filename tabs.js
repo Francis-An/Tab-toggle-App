92f53bc63723e2296f7f54298995d58e30da7ea0
Francis Annor <64334747+Francis-An@users.noreply.github.com>
2021-10-25 04:03:11 -1200

    first changes to tap toggle app

---
+++ tabs.js
@@ -0,0 +1,23 @@
+function openCity(evt,cityName){
+    //Declare all variables
+    let i, tabcontent,tablinks;
+    
+    //Get all elements with class name "tabcontent" and remove them
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for(i = 0; i < tabcontent.length; i++){
+        tabcontent[i].style.display = "none";
+    }
+
+    //Get all elements with class name "tablinks" and remove the "active" class
+    tablinks = document.getElementsByClassName('tablinks');
+    for(i = 0; i < tablinks.length; i++){
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    //Show 
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+}
+//Default open
+document.getElementById('default').click();
